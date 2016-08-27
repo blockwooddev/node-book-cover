161ed9796ff2f0b12362e697769c1678319bee82
Barry <barrylockwood.swdev@gmail.com>
2016-08-27 00:55:08 -0700

    Issue 1: Made some changes to output SVG, but this only seems to work with certain versions of virtual-dom (https://github.com/nthtran/vdom-to-html/issues/13). I'm still working on handling that.

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+let fs = require('fs');
 var Rune = require('rune.js');
-var r = new Rune();
-console.log(r);
+var VNode = require('virtual-dom/vnode/vnode');
+var toHTML = require('vdom-to-html');
+
+var r = new Rune({
+    width: 640, 
+    height: 480
+});
+
+var radius = 100;
+var points = 4;
+
+
+var shape = r.polygon(r.width/2, 125);
+var spacing = 360/points;
+
+for(var j = 0; j < points; j++) {
+  var x = Math.cos(Rune.radians(j * spacing)+6) * radius;
+  var y = Math.sin(Rune.radians(j * spacing)+6) * radius;
+  shape.lineTo(x, y);
+}
+
 r.draw();
-// draw shapes
-console.log(r.getEl());
+
+fs.writeFileSync('out/out.svg', toHTML(r.tree), 'utf8');
+
