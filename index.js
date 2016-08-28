3b20d93850b998c2666c2b9cddabbf7672da7471
Barry <barrylockwood.swdev@gmail.com>
2016-08-28 01:25:14 -0700

    Started using my design from the designs folder, makes it easier to swap out ideas.

--- index.js
+++ index.js
@@ -2,26 +2,14 @@ let fs = require('fs');
 var Rune = require('rune.js');
 var VNode = require('virtual-dom/vnode/vnode');
 var toHTML = require('vdom-to-html');
+var Design1 = require('./designs/design1');
 
 var r = new Rune({
     width: 640, 
     height: 480
 });
 
-var radius = 100;
-var points = 4;
-
-
-var shape = r.polygon(r.width/2, 125);
-var spacing = 360/points;
-
-for(var j = 0; j < points; j++) {
-  var x = Math.cos(Rune.radians(j * spacing)+6) * radius;
-  var y = Math.sin(Rune.radians(j * spacing)+6) * radius;
-  shape.lineTo(x, y);
-}
-
-r.draw();
+Design1.draw(r);
 
 fs.writeFileSync('out/out.svg', toHTML(r.tree), 'utf8');
 
