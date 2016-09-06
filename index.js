35d57b91c71c8faf43bfb1c639d4ff1390581d4e
Barry <barrylockwood.swdev@gmail.com>
2016-09-05 21:56:41 -0700

    A little bit of cleanup. Still haven't solved the problem with virtualdom when I convert the tree to html.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let fs = require('fs');
 var Rune = require('rune.js');
 var VNode = require('virtual-dom/vnode/vnode');
 var toHTML = require('vdom-to-html');
-const pnfs = require("pn/fs"); // https://www.npmjs.com/package/pn
 const svg2png = require("svg2png");
 var Design1 = require('./designs/design1');
 
@@ -13,8 +12,9 @@ var r = new Rune({
 
 Design1.draw(r);
 
-fs.writeFileSync('out/out.svg', toHTML(r.tree), 'utf8');
-pnfs.readFile("out/out.svg")
-.then(svg2png)
+var convertedSVG = toHTML(r.tree);
+console.log(convertedSVG);
+
+svg2png(new Buffer(convertedSVG), {width: 640, height: 480})
 .then(buffer => fs.writeFile("out/dest.png", buffer))
 .catch(e => console.error(e));
